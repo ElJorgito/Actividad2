fce62d79faa194913f0bc689a8fae93a4605c387
ElJorgito <11655449@alu.murciaeduca.es>
2025-11-04 15:57:38 +0100

    Initial commit

---
+++ src/main/java/es/cifpcarlos3/Actividad2_1/App.java
@@ -0,0 +1,21 @@
+package es.cifpcarlos3.Actividad2_1;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion = sc.nextInt();
+        do {
+            System.out.println("Elige una opcion: ");
+            System.out.println();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+            }
+
+        }while (opcion != 4);
+    }
+}
